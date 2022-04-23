636d05b2af5ef9d9c08dfb36955c043fd69294c3
Anna <anna_net01@mail.ru>
2022-04-23 13:15:00 +0300

    add  lr

---
+++ lr01-batch-user-reg/generate_users_list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir=`dirname $0`
+result_file="$dir/users_list.txt"
+
+count_users=500
+
+rm $result_file
+
+for i in $(seq -w 1 $count_users); do
+
+login="user&i"
+password=$(pwgen -s 8 1)
+line = "$login,$password"
+echo "$line">>$result_file
+
+done
