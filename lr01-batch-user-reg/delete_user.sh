db4a74df9723f86dc3ce99a46d20f9e427e01720
Anna <anna_net01@mail.ru>
2022-04-30 08:46:52 +0300

    add  lr

--- lr01-batch-user-reg/delete_user.sh
+++ lr01-batch-user-reg/delete_user.sh
@@ -4,28 +4,22 @@ dir=`dirname $0`
 input_file="$dir/users_list.txt"
 
 back_to_dir="/opt"
-backup_name=$(echo `LANG=C date` | awk -F ' |:' {'print"backup_"$2"_"$3"_"$8"_"$4"_"$5"_"$6'})
-back_path="$back_to_dir/$backup_name.tar"
+back_path="$back_to_dir/reserve.tar"
 
-for i in $(cat $input_file); do
+for i in `cat $input_file`; do
 login=$(echo "$i" | awk -F ',' {'print $1'})
 
-if [ "$1" == "with-backup" ];
-then
-tar --directory="/home" -rf $back_path $login &>>/dev/null
+tar --directory="/home" -rf $back_path $login &>> /dev/null
 
-if [ $? -ne 0 ];
-then
+if [ $? -ne 0 ];then
 echo "ERROR: user directory doesn't exist"
 else
 echo "Backup was done successfully"
 fi
-fi
 
 deluser --remove-home "$login" &>>/dev/null
 
-if [ $? -ne 0 ];
-then
+if [ $? -ne 0 ];then
 echo  "ERROR: $login doesn't exist"
 else
 echo "$login was deleted successfully"
